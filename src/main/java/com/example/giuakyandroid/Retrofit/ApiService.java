0d2826fd45c53f2d437aebfed93d3b583708475b
tranthuyvy <ttv.thuyvy.1544@gmail.com>
2023-05-18 17:28:07 +0700

    giuaky

---
+++ src/main/java/com/example/giuakyandroid/Retrofit/ApiService.java
@@ -0,0 +1,52 @@
+package com.example.giuakyandroid.Retrofit;
+
+import com.example.giuakyandroid.Model.Order;
+import com.example.giuakyandroid.Model.Product;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.OkHttpClient;
+import retrofit2.Call;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.Body;
+import retrofit2.http.GET;
+import retrofit2.http.PUT;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
+
+
+
+public interface ApiService {
+
+    String token = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJxdWFuZ2h1bmciLCJpYXQiOjE2ODQzMTkwNzAsImV4cCI6MTY4NDQwNTQ3MH0.t671hdtmJDe4uK5BdUYB3VwGr7obIe3yLiBy6Fjq7Ey1HsFqDNHJXKTNsMov5FoZfyHy0Ls5s3ajWSeinbXJFg";
+    OkHttpClient client = new OkHttpClient.Builder()
+            .connectTimeout(100, TimeUnit.SECONDS)
+            .readTimeout(100,TimeUnit.SECONDS)
+            .addInterceptor(new TokenInterceptor(token))
+            .build();
+    Gson gson = new GsonBuilder()
+            .setDateFormat("yyyy-MM-dd HH:mm:ss")
+            .create();
+
+    // client -> setTime gọi API,
+//https://d19cqcnpm01-api.azurewebsites.net/
+    ApiService apiService = new Retrofit.Builder()
+            .baseUrl("https://d19cqcnpm01-api.azurewebsites.net/")
+            .client(client)
+            .addConverterFactory(GsonConverterFactory.create(gson))
+            .build()
+            .create(ApiService.class);
+
+    @GET("api/orders")
+    Call<List<Order>> getOrdersfromUserId(@Query("userId") int userId );
+    @PUT("api/orders/{orderId}")
+    Call<Order> PutOrderByOrderID(@Path("orderId") int orderId, @Body Order order );
+
+    @GET("api/products")
+    Call<List<Product>> productListData();
+
+}
